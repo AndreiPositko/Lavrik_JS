b2fe677ad811ef1e8f3814fbe9d78bc04e8eabaa
PositkoAndrei <positkoandrei@gmail.com>
2020-09-08 07:12:51 +0300

    slider oop class

--- js_lesson2/8_slider_idea/js/script.js
+++ js_lesson2/8_slider_idea/js/script.js
@@ -1,37 +1,69 @@
 window.onload = function () {
     
-    var slider = new Slider({
-        images: '.gallery-1 img',
+    let slider1 = new Slider({
+        images: '.gallery-1 .photos img',
         btnPrev: '.gallery-1 .buttons .prev',
         btnNext: '.gallery-1 .buttons .next',
         auto: false
-    });
+    })
     
-}
+    let slider2 = new Slider({
+        images: '.gallery-2 .photos img',
+        btnPrev: '.gallery-2 .buttons .prev',
+        btnNext: '.gallery-2 .buttons .next',
+        auto: true,
+        rate: 300
+    })
+    
+    function Slider(obj) {
 
-function Slider(images) {
-    this.images = images;
-    var i = 0;
+        this.images = document.querySelectorAll(obj.images);
+        // this.btnPrev = obj.btnPrev;
+        // this.btnNext = obj.btnNext;
 
-    this.prev = function () {
-        this.images[i].classList.remove('showed');
-        i--;
+        this.btnPrev = document.querySelector(obj.btnPrev);
+        this.btnNext = document.querySelector(obj.btnNext);
+        this.auto = obj.auto;
+        this.rate = obj.rate || 1000;
+        console.log(this.rate);
 
-        if (i < 0) {
-            i = this.images.length - 1;
+        let slider = this;
+        console.log(slider.btnPrev);
+
+        let i = 0;
+
+        this.prev = function() {
+            slider.images[i].classList.remove('showed');
+            i--;
+
+            if (i < 0) {
+                i = slider.images.length - 1;
+            }
+            slider.images[i].classList.add('showed');
         }
 
-        this.images[i].classList.add('showed');
-    }
+        console.log(slider);
 
-    this.next = function () {
-        this.images[i].classList.remove('showed');
-        i++;
+        this.next = function() {
+            slider.images[i].classList.remove('showed');
+            i++;
 
-        if (i >= this.images.length) {
-            i = 0;
+            if (i >= slider.images.length) {
+                i = 0;
+            }
+            slider.images[i].classList.add('showed');
         }
 
-        this.images[i].classList.add('showed');
+        slider.btnPrev.onclick = function() {
+            slider.prev();
+        }
+        
+        slider.btnNext.onclick = function() {
+            slider.next();
+        }
+
+        if (slider.auto) {
+            setInterval(slider.prev, slider.rate);
+        }
     }
 }
